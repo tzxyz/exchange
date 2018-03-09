749d05f77d13e2b4a040d396241f670724b74e26
zhuonima <tz.zhuo@protonmail.com>
2018-03-10 02:14:33 +0800

    定义用户模型

--- exchange-users/src/main/java/org/zhuonima/exchange/orders/models/User.java
+++ exchange-users/src/main/java/org/zhuonima/exchange/orders/models/User.java
@@ -2,6 +2,18 @@ package org.zhuonima.exchange.orders.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "exchange_users", indexes = @Index(columnList = "email", unique = true))
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    private String password;
 }
---
+++ exchange-users/src/main/java/org/zhuonima/exchange/orders/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package org.zhuonima.exchange.orders.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.zhuonima.exchange.orders.models.User;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {}
